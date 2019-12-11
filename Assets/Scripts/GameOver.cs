b69c1f718aeb6f278114e5a0cd883cefaf6f5edb
saiteja0224 <saitejakonda224@gmail.com>
2019-12-11 15:35:44 -0500

    Win_Lose Animation

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class GameOver : MonoBehaviour
 {
+    public GameObject gameWin;
+    public GameObject gameLose;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
         if (collision.gameObject.name == "Food" && collision.gameObject.transform.position.x > 0)
         {
             Debug.Log("You Lose");
+            gameLose.SetActive(true);
         }
         else if (collision.gameObject.name == "Food" && collision.gameObject.transform.position.x < 0)
         {
             Debug.Log("You Win");
+            gameWin.SetActive(true);
         }
     }
 }
---
+++ Assets/startgame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startgame : MonoBehaviour
+{
+    public Animator animator;
+    public void PlayGame()
+    {
+        FadetoLevel();
+
+        //SceneChange();
+    }
+
+    public void FadetoLevel()
+    {
+
+        animator.SetTrigger("FadeOut");
+
+    }
+    public void SceneChange()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
